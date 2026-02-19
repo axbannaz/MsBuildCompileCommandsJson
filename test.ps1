ec14fba617b2dc94d4f750efd5530e2d18ac15e2
Arash Bannazadeh-Mahani <116259+axbannaz@users.noreply.github.com>
2026-02-18 21:46:04 -0800

    Style

--- test.ps1
+++ test.ps1
@@ -6,6 +6,9 @@ Set-Location "${PSScriptRoot}"
 
 & .\build.ps1
 
-& pwsh -nop -WorkingDirectory (Get-Location) -C { findvs ${env:PROCESSOR_ARCHITECTURE}; msbuild -logger:./bin/Debug/net481/CompileCommandsJson.dll test/dir.proj }
+& pwsh -nop -WorkingDirectory (Get-Location) -C {
+	findvs ${env:PROCESSOR_ARCHITECTURE}
+	msbuild -logger:./bin/Debug/net481/CompileCommandsJson.dll test/dir.proj
+}
 
 Set-Location "${oldPwd}"
